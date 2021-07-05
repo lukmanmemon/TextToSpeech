7d1092911175b2355c554a765949942c499831fb
lukmanmemon <memon_lukman@hotmail.com>
2021-07-05 19:11:14 -0400

    Added import text file feature

--- main.py
+++ main.py
@@ -1,22 +1,33 @@
-from tkinter import Tk, Text, Entry, Button
-
+from tkinter import Tk, Text, Entry, Button, ttk, messagebox, filedialog, INSERT
+import playsound
 # Import module for text to speech conversion
 from gtts import gTTS
-# Module to play converted audio
-import os
+
 
 gui = Tk()
 gui.geometry("750x300")
+gui.title("Text to speech")
 
 text_box = Text(gui, height = 15, width = 60)
 text_box.pack()
 
 def retrieve_input():
-    input = text_box.get("1.0", "end-1c") # This is the text you may want to use later
+    input = text_box.get("1.0", "end")
     return input
 
-b = Button(gui, text = "OK", width = 10, command = gui.quit)
-b.pack()
+read_button = Button(gui, text = "Read", width = 10, bg = "orange", command = gui.quit)
+read_button.pack()
+
+def import_file():
+    filename = filedialog.askopenfilename(initialdir = "/", title = "Select file", filetypes = ([("text files", ".txt"), ("DOCX", ".docx")]))
+    txt_file = open(filename, "r")
+    contents = txt_file.read()
+    txt_file.close()
+    text_box.insert(INSERT, contents)
+    return contents
+
+add_file = Button(gui, text = "Import file", width = 10, command = import_file)
+add_file.pack()
 
 gui.mainloop()
 
@@ -24,6 +35,6 @@ mytext = retrieve_input()
 language = 'en'
 
 myobj = gTTS(text=mytext, lang=language, slow=False)
-myobj.save("test.mp3")
-# Playing converted file 
-os.system("start test.mp3")
+myobj.save("text.mp3")
+
+playsound.playsound("text.mp3")
