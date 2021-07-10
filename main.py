fd1843a4fc496243cc3dd8c5c5802ea5c6aa89e3
lukmanmemon <memon_lukman@hotmail.com>
2021-07-10 17:12:41 -0400

    Changes to UI

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
-from tkinter import Tk, Text, Entry, Button, ttk, messagebox, filedialog, INSERT
+from tkinter import Tk, Text, Entry, Button, ttk, messagebox, filedialog, INSERT, LabelFrame
 import os
 import playsound
 # Import module for text to speech conversion
 from gtts import gTTS
 
 gui = Tk()
-gui.geometry("750x600")
-gui.title("Text to speech")
+width = gui.winfo_screenwidth()
+height = gui.winfo_screenheight()
+
+gui.geometry("%dx%d" % (width, height))
+gui.title("Text To Speech")
 
 text_box = Text(gui, height = 20, width = 80, undo = True, autoseparators = True, maxundo = -1)
 text_box.pack(pady = 10)
@@ -29,24 +32,24 @@ def process_input():
         os.remove("speech.mp3")
 
 read_button = Button(gui, text = "Read text", width = 15, height = 2, bg = "#f1bb5b", command = process_input)
-read_button.pack(pady = 10)
+read_button.place(x = width * 0.47, y = height * 0.4)
 
-def import_file():
-    filename = filedialog.askopenfilename(initialdir = "/", title = "Select file", filetypes = (("Plain text", "*.txt"),))
-    txt_file = open(filename, "r")
-    contents = txt_file.read()
-    txt_file.close()
+def upload_file():
+    filename = filedialog.askopenfilename(initialdir = "/", title = "Select file", filetypes = (("Plain text", ".txt"),))
+    file = open(filename, "r")
+    contents = file.read()
+    file.close()
 
     if text_box.get("1.0", "end") != "\n":
         clear_input()
 
     text_box.insert(INSERT, contents)
 
-add_file = Button(gui, text = "Import file", width = 15, height = 2, command = import_file)
-add_file.pack()
+add_file = Button(gui, text = "Upload file", width = 15, height = 2, command = upload_file)
+add_file.place(x = width * 0.37, y = height * 0.4)
 
 clear_button = Button(gui, text = "Clear", width = 15, height = 2, bg = "#f15b6c", command = clear_input)
-clear_button.pack(pady = 10)
+clear_button.place(x = width * 0.57, y = height * 0.4)
 
 def on_closing():
     if messagebox.askokcancel("Quit", "Are you sure you want to quit?"):
